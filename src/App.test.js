e42629cb88a34f5e8f0951ff4c792f23118fc5ae
sunilkumarsistla <sunilkumar.sistla@gmail.com>
2018-09-06 22:21:35 +0530

    updatd test window

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(
+    (<Provider store={configureStore()}>
+        <App />
+    </Provider>),
+ document.getElementById('root'));
+ ReactDOM.unmountComponentAtNode(div);
 });
